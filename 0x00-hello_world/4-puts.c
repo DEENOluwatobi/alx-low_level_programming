e1e5bb4de5f7dd0bda0db53717f3133105496a72
Oluwatobi Shodimu <shodimutobie@gmail.com>
2022-09-08 15:38:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
-
-puts ("\"Programming is like building a multilingual puzzle")
-return 0;
+int main()
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
