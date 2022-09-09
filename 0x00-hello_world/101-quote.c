86c3f24a264c0352013063e023503c4b9048c296
Oluwatobi Shodimu <shodimutobie@gmail.com>
2022-09-08 17:16:08 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 * File: 101-quote.c
 * Auth: Brennan D Baraban
 */
-
 #include <unistd.h>
-
 /**
-* main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by a new line, to standard error.
+* main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", 
+* 	followed by a new line, to standard error.
 *
 * Return: Always 1.
 */
