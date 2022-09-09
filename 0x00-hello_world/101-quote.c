03458e565a49b7cf043d4a41575311bfc5748b14
Oluwatobi Shodimu <shodimutobie@gmail.com>
2022-09-08 17:21:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 */
 #include <unistd.h>
 /**
-* main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", 
+*main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
 * followed by a new line, to standard error.
 *
 * Return: Always 1.
