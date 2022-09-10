304b38933131e8fb4bd1e3cf5b793a838844832b
Oluwatobi Shodimu <shodimutobie@gmail.com>
2022-09-09 17:48:29 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,31 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
-
-
 int main(void)
-
 {
-
 	char low;
 
-
-
 	for (low = 'z'; low >= 'a'; low--)
-
 		putchar(low);
-
 		putchar('\n');
 
-
-
 	return (0);
-
 }
