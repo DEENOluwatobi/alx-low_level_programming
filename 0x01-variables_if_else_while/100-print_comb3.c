6b231de78aff171d802a60072807fa285083f7db
Oluwatobi Shodimu <shodimutobie@gmail.com>
2022-09-09 17:37:42 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
-
-
 
 /**
  * main - Entry point
@@ -12,54 +8,29 @@
  * Return: Always 0 (Success)
  */
 
-
-
 int main(void)
-
 {
-
 	int d, p;
 
-
-
 	for (d = '0'; d < '9'; d++)
-
 	{
-
 		for (p = d + 1; p <= '9'; p++)
-
 		{
-
 			if (p != d)
-
 			{
-
 				putchar(d);
-
 				putchar(p);
 
-
-
 				if (d == '8' && p == '9')
-
 					continue;
 
-
-
 				putchar(',');
-
 				putchar(' ');
-
 			}
-
 		}
-
 	}
 
 	putchar('\n');
 
-
-
 	return (0);
-
 }
