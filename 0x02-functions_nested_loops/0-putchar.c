7f526c8ec1bc2f850082522b6d36a2ffef27e91b
Oluwatobi Shodimu <shodimutobie@gmail.com>
2022-09-14 17:10:16 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	char word[7] = "putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
