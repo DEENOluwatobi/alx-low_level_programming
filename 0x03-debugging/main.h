7058656af20ddc265e109f01e6c061d92a56d0e6
DEENOluwatobi <shodimutobie@gmail.com>
2022-09-16 11:36:23 +0100

    Debug

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H_INCLUDED
 
 #include <stdio.h>
-
+ 
 void positive_or_negative(int);
 int largest_number(int, int, int);
-void print_remaning_days(int, int, int);
+void print_remaning_days(int day, int month, int year);
 int convert_day(int, int);
 
 #endif
